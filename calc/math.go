44ba4d062c0892f77c80e0a0fdc0a3ee5bb7587e
Felipe C. Meirelles <fcmeirelles@gmail.com>
2020-05-26 22:12:39 +0200

    New release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
+import "errors"
+
 // Add returns the sum of two numbers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error, int) {
 	sum := 0
+
+	if len(numbers) < 2 {
+		return errors.New("provide more than 2 arguments"), sum
+	}
 
 	for _, num := range numbers {
 		sum = sum + num
 	}
 
-	return sum
+	return nil, sum
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/flipao/nummanip
+module github.com/flipao/nummanip/v2
 
 go 1.14
